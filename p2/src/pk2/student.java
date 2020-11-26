8419cde7dfd76a9da349ba7d581ac22d79776e6a
neketa30 <nikita.girdhar33@gmail.com>
2020-11-26 18:59:45 +0530

    Commit the code

---
+++ p2/src/pk2/student.java
@@ -0,0 +1,16 @@
+package pk2;
+
+import pk1.teacher;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		teacher t= new teacher();
+		t.method2();
+		//student obj= new student();
+
+
+	}
+
+}
